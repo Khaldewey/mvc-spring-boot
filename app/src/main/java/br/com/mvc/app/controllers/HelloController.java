4a9031dbf457dbcaaa0e0b0ea5227c19608431e3
Israel <israel_alves77@hotmail.com>
2023-12-08 16:27:15 -0300

    implementação de três diferentes requisições server-side no controle HelloController

--- app/src/main/java/br/com/mvc/app/controllers/HelloController.java
+++ app/src/main/java/br/com/mvc/app/controllers/HelloController.java
@@ -1,12 +1,32 @@
 package br.com.mvc.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloController {
-  @GetMapping("hello")
-  public String hello() {
+
+  @GetMapping("hello-model-view")
+  public ModelAndView hello() {
+    ModelAndView mv = new ModelAndView("hello");
+    mv.addObject("nome", "Gregório");
+    return mv;
+  }
+
+  @GetMapping("hello-servlet")
+  public String hello(HttpServletRequest request) {
+    request.setAttribute("nome", "João");
+    return "hello"; // o sprint vai renderizar o arquivo hello.html que está dentro de
+                    // resources/templates
+  }
+
+  @GetMapping("hello-model")
+  public String hello(Model model) {
+    model.addAttribute("nome", "Israel");
     return "hello"; // o sprint vai renderizar o arquivo hello.html que está dentro de
                     // resources/templates
   }
